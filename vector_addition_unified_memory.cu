49c04f6d9fd3a2bca314e582738fdba9fe0e8598
aydinke16@itu.edu.tr <aydinke16@itu.edu.tr>
2023-05-26 10:55:42 +0300

    unified memory with prefetch

--- vector_addition_unified_memory.cu
+++ vector_addition_unified_memory.cu
@@ -4,9 +4,9 @@
 #include <cmath>
 
 // CUDA kernel for vector addition
-__global__ void vectorAdd(int *a, int *b, int *c, int N)
+__global__ void vectorAddUM(int *a, int *b, int *c, int N)
 {
-  // blockDim: size of the thread block
+    // blockDim: size of the thread block
 	// blockIdx : id of the block
 	// threadIdx: offset of the thread inside the block
 	// Calculate global thread thread ID
@@ -15,26 +15,31 @@ __global__ void vectorAdd(int *a, int *b, int *c, int N)
 	// Boundary check
 	if (tid < N)
 	{
-		c[tid] = a[tid] + b[tid]
+		c[tid] = a[tid] + b[tid];
 	}
 }
 
 int main()
 {
+    // Get the device ID for other CUDA calls
+    int id = cudaGetDevice(&id);
+
 	// Array size of 2^16 
-	const int N = pow(2, 16);
-	size_t bytes = N * sizeof(int);
+	const int n = pow(2, 16);
+	size_t bytes = n * sizeof(int);
 
 	// Declare unified memory pointers
 	int *a, *b, *c;
 
 	// Allocation memory for these pointers
+    // Unified memory is a powerful thing 
+    // when we don't have to explicitly manage memory 
 	cudaMallocManaged(&a, bytes);
 	cudaMallocManaged(&b, bytes);
 	cudaMallocManaged(&c, bytes);
 	
 	// Initialize vectors
-	for(int i=0; i<N; i++)
+	for(int i=0; i<n; i++)
     {
         a[i] = rand() % 100;
         b[i] = rand() % 100;
@@ -44,16 +49,25 @@ int main()
     int BLOCK_SIZE = pow(2, 10);
 
     // CTAs per Grid
-    int GRID_SIZE = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    int GRID_SIZE = (int)ceil(n / BLOCK_SIZE);
 
     // Call CUDA kernel
-    vectorAdd<<<GRID_SIZE, BLOCK_SIZE>>>(a,b,c,N);
+    // GPU starts up and it says i dont have any of this data
+    // So it does something called a page fault(page in memory from CPU to GPU)
+    // We can hints to say when data should be where
+    // We do this by prefetching
+    cudaMemPrefetchAsync(a, bytes, id);
+    cudaMemPrefetchAsync(b, bytes, id);
+    vectorAddUM<<<GRID_SIZE, BLOCK_SIZE>>>(a,b,c,n);
 
     // Wait for all previous operations before using values
     cudaDeviceSynchronize();
 
+    // We can also prefetch the result back to CPU
+    cudaMemPrefetchAsync(c, bytes, cudaCpuDeviceId);
+
     // Verify the result on the CPU
-    for(int i=0; i<N; i++)
+    for(int i=0; i<n; i++)
     {
         assert(c[i] == a[i] + b[i]);
     }
